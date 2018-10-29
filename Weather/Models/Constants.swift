74dfc79a8d2773b366f7fdadd15d93fc61fc46d7
Anıl Göktaş <goktas.anil@gmail.com>
2018-10-29 12:01:24 +0300

    Add OpenWeatherMap api key

---
+++ Weather/Models/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Anıl Göktaş on 29.10.2018.
+//  Copyright © 2018 Anıl Göktaş. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    
+    enum OpenWeatherMap {
+        static let apiKey = "0f96771d06ee3a4ea796db88f7381cc8"
+    }
+    
+}
