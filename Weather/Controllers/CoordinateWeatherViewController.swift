21cec773de61224bd733b7e18a0c275a786aabe1
Anıl Göktaş <goktas.anil@gmail.com>
2018-10-29 15:15:55 +0300

    Assign UI elements to CoordinateWeather VC and add retry logic

--- Weather/Controllers/CoordinateWeatherViewController.swift
+++ Weather/Controllers/CoordinateWeatherViewController.swift
@@ -14,14 +14,15 @@ final class CoordinateWeatherViewController: UIViewController {
     
     // MARK: - IBOutlets
     
-    @IBOutlet fileprivate weak var locationLabel: UILabel!
-    @IBOutlet fileprivate weak var titleLabel: UILabel!
-    @IBOutlet fileprivate weak var imageView: UIImageView!
-    @IBOutlet fileprivate weak var descriptionLabel: UILabel!
-    @IBOutlet fileprivate weak var temperatureLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var temperatureLabel: UILabel!
     
     // MARK: - Properties
     
+    /// Presenter View Controller must assign this property in order to setup.
     var locationCoordinate: CLLocationCoordinate2D! { didSet { didSetLocationCoordinate() } }
     private var weather = Weather()
     private var currentState = State.loading { didSet { didSetCurrentState() } }
@@ -35,16 +36,9 @@ final class CoordinateWeatherViewController: UIViewController {
     }
     
     deinit {
-        // Cancel data request if not completed yet.
+        // Cancel data request if it's not completed yet.
         weather.dataRequest?.cancel()
     }
-    
-}
-
-// MARK: - IBActions
-
-extension CoordinateWeatherViewController {
-    
     
 }
 
@@ -65,21 +59,26 @@ extension CoordinateWeatherViewController: Configurable {
     private func didSetCurrentState() {
         switch currentState {
         case .loading:
+            // Loading state.
             titleLabel.text = "Loading..."
             descriptionLabel.text = "Please wait."
             locationLabel.text = " "
+            navigationItem.setRightBarButton(nil, animated: true)
         case .error:
+            // Error state.
             titleLabel.text = "Something went wrong."
             locationLabel.text = " "
             descriptionLabel.text = "Please try again."
-            temperatureLabel.text = " "
-            // retry button.
+            temperatureLabel.text = "..."
+            navigationItem.setRightBarButton(retryRightBarButtonItem(), animated: true)
         case .success:
+            // Success state.
             locationLabel.text = weather.locationName
             titleLabel.text = weather.title
             descriptionLabel.text = weather.descriptionString.capitalized
             temperatureLabel.text = String(weather.temperature)
             imageView.kf.setImage(with: weather.iconURL)
+            navigationItem.setRightBarButton(nil, animated: true)
         }
     }
     
@@ -92,4 +91,25 @@ extension CoordinateWeatherViewController: Configurable {
         }
     }
     
+}
+
+// MARK: - Retry
+
+extension CoordinateWeatherViewController {
+    
+    // Factory method for assigning right bar button item.
+    private func retryRightBarButtonItem() -> UIBarButtonItem {
+        return UIBarButtonItem(title: "Retry", style: .plain, target: self, action: .retry)
+    }
+    
+    @objc func retry() {
+        didSetLocationCoordinate()
+    }
+    
+}
+
+// MARK: - Selector
+
+private extension Selector {
+    static let retry = #selector(CoordinateWeatherViewController.retry)
 }
